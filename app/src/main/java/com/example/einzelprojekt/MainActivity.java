24ee5ec399f060cc38a32b135be1e225c1c8eac8
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2023-03-08 13:03:42 +0100

    btn , output, input variables created and linked to their components

--- app/src/main/java/com/example/einzelprojekt/MainActivity.java
+++ app/src/main/java/com/example/einzelprojekt/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.einzelprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Button btn = findViewById(R.id.button);
+    TextView output = findViewById(R.id.result);
+    EditText input = findViewById(R.id.editTextNumber);
+
 }
