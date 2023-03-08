b75b4d21a633f4178c68bfdb06898543b4553357
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2023-03-08 15:37:42 +0100

    onclicklistener created for the button

--- app/src/main/java/com/example/einzelprojekt/MainActivity.java
+++ app/src/main/java/com/example/einzelprojekt/MainActivity.java
@@ -8,18 +8,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        TextView output = findViewById(R.id.result);
+        EditText input = findViewById(R.id.editTextNumber);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectionThread cT = new ConnectionThread (input.getText().toString());
+                cT.start();
+            }
+        });
+
+
     }
-
-    Button btn = findViewById(R.id.button);
-    TextView output = findViewById(R.id.result);
-    EditText input = findViewById(R.id.editTextNumber);
-
-  //  btn.OnClickListener
-
 }
